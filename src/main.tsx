797842084f26531e2124c9fe6ee6cae6592d4be3
uranny <164876325+uranny@users.noreply.github.com>
2026-03-09 16:00:00 +0900

    refactor : build file js to ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/QueryClient.js'
+import { queryClient } from './lib/QueryClient'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
