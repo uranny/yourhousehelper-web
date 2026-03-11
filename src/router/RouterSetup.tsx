70a2524579e4a45944b9fc7e6f89060598149738
uranny <164876325+uranny@users.noreply.github.com>
2026-03-11 19:41:16 +0900

    fix : overlap router

--- src/router/RouterSetup.tsx
+++ src/router/RouterSetup.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Table from '../pages/Table';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Start from '../pages/Start';
+import Analysis from '../pages/Analysis';
 
 function RouterSetup() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Start/>} />
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/table" element={<Table />}/>
-        <Route path='/signin' element={<Signin/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Start/>} />
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/table" element={<Table />}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/table/analysis' element={<Analysis/>}/>
+    </Routes>
   );
 }
 
