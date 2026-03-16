f8d7d63fe803d6bf7f62b5a819522ed95d4c012f
uranny <164876325+uranny@users.noreply.github.com>
2026-03-16 10:31:55 +0900

    refactor : create typography constants

---
+++ src/constants/typography.ts
@@ -0,0 +1,40 @@
+import { css } from "styled-components";
+
+export const bodyText = css`
+  font-size: 1.6rem;
+  font-weight: 400;
+
+  @media (max-width: 1024px) {
+    font-size: 1.4rem;
+  }
+
+  @media (max-width: 512px) {
+    font-size: 1.2rem;
+  }
+`;
+
+export const titleText = css`
+  font-size: 2.8rem;
+  font-weight: 800;
+
+  @media (max-width: 1024px) {
+    font-size: 2.6rem;
+  }
+
+  @media (max-width: 512px) {
+    font-size: 2.4rem;
+  }
+`;
+
+export const subtitleText = css`
+  font-size: 2.2rem;
+  font-weight: 600;
+
+  @media (max-width: 1024px) {
+    font-size: 2.0rem;
+  }
+
+  @media (max-width: 512px) {
+    font-size: 1.8rem;
+  }
+`;
